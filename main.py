1dfbb29d1011110518c08c7fc4a3f96793ac276c
Everton Espedito <evertondev505@gmail.com>
2025-08-31 00:43:23 -0300

    Update main.py
    Funções básicas de togle e menus

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PySide6.QtCore import QCoreApplication
+from PySide6.QtCore import QCoreApplication, QPropertyAnimation, QEasingCurve
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
 from PySide6.QtGui import QIcon
 from ui_main import Ui_MainWindow
@@ -10,7 +10,36 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle("Sistema de Cadastro de Empresas")
         appIcon = QIcon(u"")
-        self.setWindowIcon(appIcon)
+        self.setWindowIcon(appIcon) # Define o ícone da janela
+
+        ###################################################
+        # Botão de menu
+        self.btn_toggle.clicked.connect(self.leftSlideMenu)
+        ###################################################
+        # PAGINAS DO SISTEMA
+        self.btn_home.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_home))
+        self.btn_menu_cadastrar.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_cadastro))
+        self.btn_menu_contatos.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_contatos))
+        self.btn_menu_sobre.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_about))
+        ###################################################
+
+
+    def leftSlideMenu(self):
+        width = self.left_menu.width()
+
+        if width == 9:
+            newWidth = 200
+        else:
+            newWidth = 9
+
+        self.animation = QPropertyAnimation(self.left_menu, b"maximumWidth")
+        self.animation.setDuration(250) # duração da animação em milissegundos
+        self.animation.setStartValue(width)
+        self.animation.setEndValue(newWidth)
+        self.animation.setEasingCurve(QEasingCurve.InOutQuart)# tipo de animação (velocidade)
+        self.animation.start()
+
+
 
 
 if __name__ == "__main__":# se o arquivo for executado diretamente
